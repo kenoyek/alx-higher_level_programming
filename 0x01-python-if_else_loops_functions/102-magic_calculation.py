5d571037657d95512fe1f48368f00b26175d61c1
Okagua Kenoye <kenoyek@gmail.com>
2022-11-29 21:16:02 +0100

    Update 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-# Author - Bamidele Adefolaju
-
 def magic_calculation(a, b, c):
-    """Match bytecode provided by Holberton School."""
     if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-    return (a*b - c)
+        return(c)
+    elif c > b:
+        return(a + b)
+    else:
+        return(a * b - c)
