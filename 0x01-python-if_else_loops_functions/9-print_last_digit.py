c17faaae18a7bb1f08348b886a561f0c88068b45
Okagua Kenoye <kenoyek@gmail.com>
2022-11-29 20:46:17 +0100

    Update 9-print_last_digit.py

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-# Author - Tolulope Fakunle
-
 def print_last_digit(number):
-    print(abs(number) % 10, end="")
-    return (abs(number) % 10)
+    if number < 0:
+        print("{:d}".format(-(number % -10)), end='')
+        return(-(number % -10))
+    else:
+        print("{:d}".format(number % 10), end='')
+        return(number % 10)
