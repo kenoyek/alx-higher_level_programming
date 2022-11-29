dc08cc59efb8397a32328771a7527981c2d9fc4d
Okagua Kenoye <kenoyek@gmail.com>
2022-11-29 21:12:17 +0100

    Update 101-remove_char_at.py

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-# Author - bamidele Adefolaju
-
 def remove_char_at(str, n):
-    if n < 0:
+    if n >= 0:
+        newstr = str[:n] + str[n + 1:]
+        return (newstr)
+    else:
         return (str)
-    return (str[:n] + str[n+1:])
